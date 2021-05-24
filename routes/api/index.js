154b0d58e579802037aed2d21078732ddbbb3d5b
Robert McShinsky <robert.mcshinsky@gmail.com>
2021-05-23 21:47:38 -0600

    Added comment section

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 const pizzaController = require("../../controllers/pizza-controller");
 const pizzaRoutes = require("./pizza-routes");
 
+const commentRoutes = require("./comment-routes");
+
 ////
 
 router.use("/pizzas", pizzaRoutes);
 
+router.use("/comments", commentRoutes);
 ////
 
 module.exports = router;
